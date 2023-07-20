3e744ae50d95951ac7f090de939aebc79f994df1
siema <60491231+sinol123@users.noreply.github.com>
2023-07-20 18:35:58 +0200

    troche porządek zrobiłem

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
-let length = 12;
+const characterPool = [ 
+    abc = ["a", "b", "c", "d", "e", "f", "g", "h", " i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+    ABC = ["A", "B", "C", "D", "E", "F", "G", "H", " I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+    numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"],
+    others = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]
+
+]
+
+let passwordParameters = [abcStatus = true, ABCstatus = true, numbersStatus = true, otherStatus = false, length = 12];
+
+function generatePassword(){
+    let password = "";
+    for(i = 0; i < passwordParameters[4]; i++){
+
+    }
+
+}
+
+const months = ["January", "February", "March", "April", "May", "June", "July"];
+
+const random = Math.floor(Math.random() * months.length);
+console.log(random, months[random]);
+
+function changeStatus(a){
+    if(passwordParameters[a] == true){
+        passwordParameters[a] = false
+    }
+    else{
+        passwordParameters[a] = true
+    }
+   }
 
 function changeLength(a){
     switch(a){
@@ -18,8 +48,8 @@ function changeLength(a){
             }
             else{
                 length--;
-            document.getElementById("lengthInfo").innerText = "długość hasła: " + length;
-            document.getElementById("range").value = length;
+                document.getElementById("lengthInfo").innerText = "długość hasła: " + length;
+                document.getElementById("range").value = length;
             }
             break
         default:
